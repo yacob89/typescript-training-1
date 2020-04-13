072fb6f3edb0037884bc1dea6a0cb23a5084ae19
Yacob Madiana <yacob.madiana@gmail.com>
2020-04-13 16:33:32 +0700

    Add example type

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Your code goes here...');
+function add(n1, n2, showResult, phrase) {
+    console.log(typeof number1);
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,23 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  console.log(typeof number1);
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
+  return n1 + n2;
+}
+
+const number1 = 5;
+//let number1: number = 5; --> Bad Practice because TS can automatically recognize the type already
+
+/* let number1: number;
+number1 = 5; */
+// Reasonable practice to define type first
+
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
